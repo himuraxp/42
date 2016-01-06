d7eaed400243bc0bbf9f98d0172409b7da3bd97d
Yohann LARBI <ylarbi@e1r7p3.42.fr>
2016-01-06 16:10:04 +0100

    fdf first try

--- project/fdf/main.c
+++ project/fdf/main.c
@@ -6,20 +6,14 @@
 /*   By: ylarbi <ylarbi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/29 22:49:51 by ylarbi            #+#    #+#             */
-/*   Updated: 2016/01/05 20:05:42 by ylarbi           ###   ########.fr       */
+/*   Updated: 2016/01/06 14:28:48 by ylarbi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./minilibx/mlx.h"
-#include <unistd.h>
+#include "fdf.h"
 
 int		main(void)
 {
-	void *mlx;
-	void *win;
-
-	mlx = mlx_init();
-	win = mlx_new_window(mlx, 1920, 1080, "42");
-	sleep(5);
+	ft_fdf();
 	return (0);
 }
