3dc6060e84bbd8e2d047e90c332017bc60f3f409
Yohann LARBI <ylarbi@e1r8p5.42.fr>
2016-01-07 20:29:22 +0100

    add color fdf

--- project/fdf/fdf.c
+++ project/fdf/fdf.c
@@ -6,28 +6,58 @@
 /*   By: ylarbi <ylarbi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/06 14:23:19 by ylarbi            #+#    #+#             */
-/*   Updated: 2016/01/07 13:53:40 by ylarbi           ###   ########.fr       */
+/*   Updated: 2016/01/07 20:27:36 by ylarbi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./minilibx/fdf.h"
 
+int		ft_degrade(void)
+{
+	int color;
+
+	color = 0x000000;
+	while (color < 0xffffff)
+	{
+		color += 0x020202;
+		printf("%d\n", color);
+	}
+	return (color);
+}
+
 void	draw(void *mlx, void *win)
 {
+	int color;
 	int	x;
 	int y;
+	int x2;
+	int y2;
+	int x3;
+	int y3;
 
-	y = 100;
-	sleep(5);
-	while (y < 200)
+	y = 400;
+	y2 = y / 4;
+	x = 400;
+	x2 = x / 4;
+	color = 0x090909;
+	while (x2 != x / 2 && y2 != y / 2)
 	{
-		x = 100;
-		while (x < 200)
+		y3 = y - y2;
+		while (y3 < y2)
 		{
-			mlx_pixel_put(mlx, win, x, y, 0xFF0000);
-			x++;
+			x3 = x - x2;
+			while (x3 < x2)
+			{
+				mlx_pixel_put(mlx, win, x, y, color);
+				x3++;
+			}
+
+			y3++;
+			if (color < 0xff00ff)
+				color += 0x020815;
 		}
-		y++;
+		x2++;
+		y2++;
 	}
 }
 
@@ -37,26 +67,26 @@ int		expose_hook(t_env *e)
 	return (0);
 }
 
-int		key_hook(int keycode, t_env *e)
+int		key_hook(int keycode)
 {
 	printf("key: %d\n", keycode);
-	if (keycode == 65307)
+	if (keycode == 53)
 		exit(0);
 	return (0);
 }
 
-int		mouse_hook(int button, int x, int y, t_env *e)
+int		mouse_hook(int button, int x, int y)
 {
 	printf("mouse: %d position: %d:%d\n", button, x, y);
 	return (0);
 }
 
-int		ft_fdf()
+int		ft_fdf(void)
 {
 	t_env	e;
 
 	e.mlx = mlx_init();
-	e.win = mlx_new_window(e.mlx, 1200, 800, "42");
+	e.win = mlx_new_window(e.mlx, 800, 800, "42");
 	mlx_key_hook(e.win, key_hook, &e);
 	mlx_mouse_hook(e.win, mouse_hook, &e);
 	mlx_expose_hook(e.win, expose_hook, &e);
---
+++ project/fdf/minilibx/fdf.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ylarbi <ylarbi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/01/07 15:53:45 by ylarbi            #+#    #+#             */
+/*   Updated: 2016/01/07 19:05:52 by ylarbi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <mlx.h>
+
+typedef struct	s_env
+{
+	void		*mlx;
+	void		*win;
+}				t_env;
+
+typedef struct	s_color
+{
+	int		r;
+	int		v;
+	int		b;
+}				t_color;
+
+int		ft_fdf(void);
+
+#endif
