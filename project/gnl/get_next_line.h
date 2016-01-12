f93314bc470da4ddf68b434163e461ce852c7d0e
Yohann Larbi <HimuraXp@Yohanns-Mac.local>
2016-01-12 14:24:23 +0100

    test gnl

--- project/gnl/get_next_line.h
+++ project/gnl/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ylarbi <ylarbi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/21 11:16:27 by ylarbi            #+#    #+#             */
-/*   Updated: 2016/01/02 16:28:21 by ylarbi           ###   ########.fr       */
+/*   Updated: 2016/01/11 10:59:03 by ylarbi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include <sys/types.h>
 # include <sys/uio.h>
 # include "./libft/libft.h"
-# define BUFF_SIZE 10
+# define BUFF_SIZE 4
 
 int		get_next_line(int const fd, char **line);
 
