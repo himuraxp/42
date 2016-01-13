7cb851b2a529c4c2b12b816dafc901ec75c833d2
Yohann LARBI <ylarbi@e1r8p5.42.fr>
2016-01-13 12:50:03 +0100

    Final GNL

--- project/fdf/fdf.c
+++ project/fdf/fdf.c
@@ -6,7 +6,7 @@
 /*   By: ylarbi <ylarbi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/06 14:23:19 by ylarbi            #+#    #+#             */
-/*   Updated: 2016/01/12 16:48:48 by ylarbi           ###   ########.fr       */
+/*   Updated: 2016/01/13 11:13:06 by ylarbi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,7 +68,7 @@ int		mouse_hook(int button, int x, int y)
 	return (0);
 }
 
-int		ft_fdf(void)
+int		ft_fdf(char **av)
 {
 	t_env	e;
 
--- project/fdf/main.c
+++ project/fdf/main.c
@@ -6,14 +6,34 @@
 /*   By: ylarbi <ylarbi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/29 22:49:51 by ylarbi            #+#    #+#             */
-/*   Updated: 2016/01/06 21:02:27 by ylarbi           ###   ########.fr       */
+/*   Updated: 2016/01/13 11:18:45 by ylarbi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./minilibx/fdf.h"
 
-int		main(void)
+int		main(int ac, char **av)
 {
-	ft_fdf();
+	char	*line;
+	int		fd;
+	int		gnl;
+
+	gnl = 0;
+	if (ac < 2)
+		return (0);
+	if (ac == 2)
+		fd = open(av[1], O_RDONLY);
+	else
+		fd = 0;
+	gnl = get_next_line(fd, &line);
+	while (gnl != 0 && gnl != -1)
+	{
+		ft_fdf(av[1]);
+		gnl = get_next_line(fd, &line);
+	}
+	if (gnl == -1)
+		ft_putstr("Error reading file.\n");
+	if	(fd != 0)
+		close(fd);
 	return (0);
 }
