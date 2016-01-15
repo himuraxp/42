f4c039d83a0b3bcd2645c10821feb92455477524
Yohann Larbi <HimuraXp@Yohanns-Mac.local>
2016-01-15 19:13:03 +0100

    update -> new config

--- project/fdf/src/draw.c
+++ project/fdf/src/draw.c
@@ -6,7 +6,7 @@
 /*   By: ylarbi <ylarbi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/14 19:18:18 by ylarbi            #+#    #+#             */
-/*   Updated: 2016/01/14 22:12:56 by ylarbi           ###   ########.fr       */
+/*   Updated: 2016/01/15 14:10:31 by ylarbi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,13 +32,29 @@ void	background(t_data *data)
     }
 }
 
+void	back(t_data *data)
+{
+    int x;
+    int y;
+
+    y = 0;
+    x = 0;
+    while (y <= HEIGHT / 2)
+    {
+        x = 0;
+        while (x <= WIDTH / 2)
+        {
+            mlx_pixel_put(data->mlx, data->win, x, y, 0x000000);
+            x++;
+        }
+        y++;
+    }
+}
+
 void    draw(t_data *data)
 {
 	background(data);
+	back(data);
 }
 
-/*int     expose_hook(t_data *data)
-{
-    draw(data->mlx, data->win);
-    return (0);
-}*/
+
