9f89f71973f1dc1e7503d4860ea122fa170e9774
Yohann Larbi <HimuraXp@Mac-Pro-de-Yohann.local>
2016-01-25 11:37:46 +0100

    modif gnl buffer a 8 & modification Makefile libft

--- project/gnl/get_next_line.h
+++ project/gnl/get_next_line.h
@@ -6,11 +6,7 @@
 /*   By: ylarbi <ylarbi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/21 11:16:27 by ylarbi            #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2016/01/11 10:59:03 by ylarbi           ###   ########.fr       */
-=======
-/*   Updated: 2016/01/13 14:33:09 by ylarbi           ###   ########.fr       */
->>>>>>> 42df8701b5f73e1443b2dab2917a95d71290a67a
+/*   Updated: 2016/01/25 11:27:36 by ylarbi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +15,8 @@
 
 # include <sys/types.h>
 # include <sys/uio.h>
-<<<<<<< HEAD
-# include "./libft/libft.h"
-# define BUFF_SIZE 4
-=======
-# include "libft/include/libft.h"
+# include "./libft/include/libft.h"
 # define BUFF_SIZE 8
->>>>>>> 42df8701b5f73e1443b2dab2917a95d71290a67a
 
 int		get_next_line(int const fd, char **line);
 
