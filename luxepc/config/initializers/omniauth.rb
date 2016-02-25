85a76244adcdc1e43958c0623ab67c1241e0740c
Yohann Larbi <HimuraXp@Mac-Pro-de-Yohann.local>
2016-02-25 10:49:47 +0100

    del luxepc

--- luxepc/config/initializers/omniauth.rb
+++
@@ -1,7 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '701490146612471', '1475d10823ec3977c5d49a5d27320b8b', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
-end
-
-
