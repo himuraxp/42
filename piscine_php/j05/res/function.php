b507da021820af2efc4c949ecf413bba8a407742
Yohann Larbi <HimuraXp@Mac-Pro-de-Yohann.local>
2016-04-11 22:30:12 +0200

    finish ex18

---
+++ piscine_php/j05/res/function.php
@@ -0,0 +1,8 @@
+<?php
+mysql_connect("root", "Eiko0962!") or die('Error : '.mysql_error());
+
+mysql_select_db("db_ylarbi");
+
+$res = mysql_query("SELECT nom FROM fiche_personne WHERE nom = 'strcmp("a", $nom[$i]).";"'");
+
+?>
