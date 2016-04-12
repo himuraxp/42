101e68f0f93e9710c7e9f4809f29ebdbf2f43b34
Yohann Larbi <HimuraXp@Mac-Pro-de-Yohann.local>
2016-04-12 21:28:17 +0200

    ex00 j06

--- piscine_php/j06/ex00/Color.class.php
+++ piscine_php/j06/ex00/Color.class.php
@@ -1,6 +1,12 @@
 <?php
 
 class Color {
+
+	public $red = 0;
+	public $green = 0;
+	public $blue = 0;
+	public $rbg = 0;
+
 	function __construct(array $kwargs) {
 		if (array_key_exists('red', $kwargs))
 			$this->red = $kwargs['red'];
@@ -14,14 +20,35 @@ class Color {
 			$this->blue = $kwargs['blue'];
 		else
 			$this->blue = 0;
-		print("red = ".$this->red."\ngreen = ".$this->green."\nblue = ".$this->blue."\n");
+		print($this->red."\n".$this->green."\n".$this->blue."\n");
 		return;
 	}
 	function __destruct() {
-		echo "destructeur\n";
+		print( 'Destructeur called' . PHP_EOL );
 		return;
+	}
+	public function __toString()
+	{
+		return $this->rgb;
 	}
 }
 
-$instance = new Color(array('red' => 255, 'green' => 255, 'blue' => 255));
+function hexa_rgb($hex) {
+	$hex = str_replace("#", "", $hex);
+
+if(strlen($hex) == 3) {
+	$red = hexdec(substr($hex,0,1).substr($hex,0,1));
+	$green = hexdec(substr($hex,1,1).substr($hex,1,1));
+	$blue = hexdec(substr($hex,2,1).substr($hex,2,1));
+} else {
+	$red = hexdec(substr($hex,0,2));
+	$green = hexdec(substr($hex,2,2));
+	$blue = hexdec(substr($hex,4,2));
+}
+$rgb = array($red, $green, $blue);
+return $rgb;
+}
+	$instance = new Color(array('red' => 168, 'green' => 15, 'blue' => 118));
+	$rgb = hexa_rgb("FFF");
+	$instance = new Color(array('red' => $rgb[0], 'green' => $rgb[1], 'blue' => $rgb[2]));
 ?>
