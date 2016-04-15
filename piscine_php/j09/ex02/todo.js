4f3b8b9594f89414f0b14a31c7ed7bc0ac9575c5
Yohann Larbi <HimuraXp@MacBook-Pro-2.local>
2016-04-15 18:27:32 +0200

    start ex02

--- piscine_php/j09/ex02/todo.js
+++ piscine_php/j09/ex02/todo.js
@@ -1,1 +1,10 @@
-
+function ft_list()
+{
+    var list = document.getElementById('ft_list');
+    list.getAttribute('id');
+    var note = document.createElement('div');
+    note.setAttribute('style', 'background: red;');
+    var_text = document.createTextNode('test');
+    note.appendChild(var_text);
+    list.appendChild(note);
+}
