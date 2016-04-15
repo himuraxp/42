afbe5d29541a53ad99581fba05c0ea194465ad4c
Yohann Larbi <HimuraXp@Mac-Pro-de-Yohann.local>
2016-04-15 19:40:14 +0200

    ex02

--- piscine_php/j09/ex02/todo.js
+++ piscine_php/j09/ex02/todo.js
@@ -1,10 +1,25 @@
+var i = 0;
+
 function ft_list()
 {
-    var list = document.getElementById('ft_list');
-    list.getAttribute('id');
-    var note = document.createElement('div');
-    note.setAttribute('style', 'background: red;');
-    var_text = document.createTextNode('test');
-    note.appendChild(var_text);
-    list.appendChild(note);
+	i++;
+	var list = document.getElementById('ft_list');
+	list.getAttribute('id');
+	var note = document.createElement('div');
+	var name = "note" + i;
+	note.setAttribute('class', 'note');
+	note.setAttribute('id', name);
+	note.setAttribute('onclick', 'del_note(name)');
+	note.setAttribute('value', 'i');
+	var_text = document.createTextNode('test' + ' ' + i);
+	note.appendChild(var_text);
+	list.appendChild(note);
+}
+
+function del_note(name)
+{
+	var list = document.getElementById('ft_list');
+	list.getAttribute('id');
+	var note = document.getElementById(name);
+	list.removeChild(note);
 }
