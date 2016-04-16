d189b662c2aa233f890beeb05a35ad54d9f04353
Yohann Larbi <HimuraXp@MacBook-Pro-2.local>
2016-04-16 08:08:56 +0200

    ex02

--- piscine_php/j09/ex02/todo.js
+++ piscine_php/j09/ex02/todo.js
@@ -2,24 +2,44 @@ var i = 0;
 
 function ft_list()
 {
-	i++;
-	var list = document.getElementById('ft_list');
-	list.getAttribute('id');
-	var note = document.createElement('div');
-	var name = "note" + i;
-	note.setAttribute('class', 'note');
-	note.setAttribute('id', name);
-	note.setAttribute('onclick', 'del_note(name)');
-	note.setAttribute('value', 'i');
-	var_text = document.createTextNode('test' + ' ' + i);
-	note.appendChild(var_text);
-	list.appendChild(note);
+    var msg = prompt("Enter votre note: ")
+    if (msg != "")
+    {
+    	i++;
+    	var list = document.getElementById('ft_list');
+    	list.getAttribute('id');
+    	var note = document.createElement('div');
+    	var name = "note" + i;
+    	note.setAttribute('class', 'note');
+    	note.setAttribute('id', 'note');
+        note.setAttribute('name', name);
+    	note.setAttribute('onclick', 'remove(\'' + name + '\')');
+    	note.setAttribute('value', 'i');
+    	var_text = document.createTextNode(msg);
+    	note.appendChild(var_text);
+    	list.appendChild(note);
+        list.insertBefore(note, list.firstChild);
+    }
 }
 
-function del_note(name)
-{
-	var list = document.getElementById('ft_list');
-	list.getAttribute('id');
-	var note = document.getElementById(name);
-	list.removeChild(note);
+function confirmation() {
+    var txt;
+    var r = confirm("Press a button!");
+    if (r == true) {
+        txt = "OK";
+    } else {
+        txt = "Cancel";
+    }
+    return txt;
+}
+
+function remove(name) {
+    var txt;
+    var r = confirm("Press a button!");
+    if (r == true) {
+        return (elem=document.getElementsByName(name)).parentNode.removeChild(elem);
+    } else {
+        txt = "Cancel";
+    }
+    return txt;
 }
