b15f580274d80be46a082f052e26c09e0380ef64
Yohann Larbi <HimuraXp@Mac-Pro-de-Yohann.local>
2016-04-16 13:17:56 +0200

    ex02 finish

--- piscine_php/j09/ex02/todo.js
+++ piscine_php/j09/ex02/todo.js
@@ -1,45 +1,69 @@
 var i = 0;
-
-function ft_list()
+function new_str()
 {
-    var msg = prompt("Enter votre note: ")
-    if (msg != "")
-    {
-    	i++;
-    	var list = document.getElementById('ft_list');
-    	list.getAttribute('id');
-    	var note = document.createElement('div');
-    	var name = "note" + i;
-    	note.setAttribute('class', 'note');
-    	note.setAttribute('id', 'note');
-        note.setAttribute('name', name);
-    	note.setAttribute('onclick', 'remove(\'' + name + '\')');
-    	note.setAttribute('value', 'i');
-    	var_text = document.createTextNode(msg);
-    	note.appendChild(var_text);
-    	list.appendChild(note);
-        list.insertBefore(note, list.firstChild);
-    }
+	var ft_list = document.getElementById('ft_list')
+	var str = prompt('What\'s the new To do ?');
+	if (str !== "" && str != null)
+		add_one(str);
 }
 
-function confirmation() {
-    var txt;
-    var r = confirm("Press a button!");
-    if (r == true) {
-        txt = "OK";
-    } else {
-        txt = "Cancel";
-    }
-    return txt;
+function add_one(str)
+{
+	var div = document.createElement('div');
+	var text = document.createTextNode(str);
+
+	div.setAttribute('id', i)
+	div.setAttribute('class', 'note');
+	div.setAttribute('onclick', 'del_str(id)')
+	div.appendChild(text);
+	ft_list.insertBefore(div, ft_list.firstChild);
+	set_cookie(i, str, 1);
+	i++;
 }
 
-function remove(name) {
-    var txt;
-    var r = confirm("Press a button!");
-    if (r == true) {
-        return (elem=document.getElementsByName(name)).parentNode.removeChild(elem);
-    } else {
-        txt = "Cancel";
-    }
-    return txt;
+function del_str(id)
+{
+	var ft_list = document.getElementById('ft_list')
+	var del = document.getElementById(id);
+
+	ft_list.removeChild(del);
+	document.cookie = id + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT";
+}
+
+function set_cookie(id, text, exdays)
+{
+	var d = new Date();
+	d.setTime(d.getTime()+(exdays*24*60*60*1000));
+	var expires = "expires="+d.toGMTString();
+	document.cookie = id + "=" + text + "; " +expires;
+}
+
+function get_cookie(cname)
+{
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+
+	for(var i=0; i<ca.length; i++)
+	{
+		var c = ca[i].trim();
+		if (c.indexOf(name)==0)
+			return c.substring(name.length,c.length);
+	}
+	return "";
+}
+
+function init()
+{
+	var all = document.cookie.split(';');
+
+	for (var x in all)
+	{
+		var id = all[x].split('=')[0];
+		if (!isNaN(parseInt(id)))
+		{
+			var text = all[x].split('=')[1];
+
+			add_one(text);
+		}
+	}
 }
