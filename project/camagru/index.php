66a080ba55717bed290a1321317758414a761070
Yohann LARBI <ylarbi@e1r6p3.42.fr>
2016-04-21 01:02:49 +0200

    modif css canagru

--- project/camagru/index.php
+++ project/camagru/index.php
@@ -19,8 +19,20 @@ header('content-type: text/html');
 					<p><b><a href="/camagru" alt="Mon compte">Mon compte</a></b></p>
 				</li>
 			</ul>
+			<!--<div id="logo">
+				<img src="logo.png" class="logo" alt="logo" style="float: right; width: 10vw;"/>
+			</div>-->
 			<div id="container" class="container">
-				
+				<div id="col-left" class="col-left">
+					<div id="cam" class="cam">
+
+					</div>
+				</div>
+				<div id="col-right" class="col-right">
+					<div id="galerie" class="galerie">
+
+					</div>
+				</div>
 			</div>
 		</div>
 	</body>
